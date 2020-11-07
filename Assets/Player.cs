0041c7176e7bb71f115ad4cd6d8af2f0a01c98ac
Wadarass <raymond.graham4087@gmail.com>
2020-11-06 20:32:49 -0800

    Initial Project Checkin
    The files needed for class.

---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Player : MonoBehaviour
+{
+    static int xAmount = 100;
+    static int yAmount = 10;
+    static float spacing = 5.0f;
+
+    [MenuItem("Gameheads/SpawnCover")]
+    static public void CreateCoverObjects()
+    {
+        GameObject cover = AssetDatabase.LoadAssetAtPath("Assets/CoverObject.prefab", typeof(GameObject)) as GameObject;
+
+        for(int i = 0; i < xAmount; i++)
+        {
+            for (int j = 0; j < yAmount; j++)
+            {
+                Vector3 pos = new Vector3(i * spacing, 0.5f, j * spacing);
+
+                GameObject.Instantiate(cover, pos, Quaternion.identity);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindClosestCover(); 
+    }
+
+    private void FindClosestCover()
+    {
+
+    }
+}
